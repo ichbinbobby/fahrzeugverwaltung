3290d482398e514b78aa3fc19851b3a83373c4ca
SaikaVA <nthanbinh@gmail.com>
2019-09-18 14:36:21 +0200

    Added JList and delete buttons

--- src/components/gui/UserInterface.java
+++ src/components/gui/UserInterface.java
@@ -19,9 +19,12 @@ public class UserInterface {
         JTextField fahrzeugTextField = new JTextField("Fahrzeug...");
         JButton addBesitzerBtn = new JButton("Hinzufügen");
         JButton addFahrzeugBtn = new JButton("Hinzufügen");
+        JButton rmBesitzerBtn = new JButton("Löschen");
+        JButton rmFahrzeugBtn = new JButton("Löschen");
 
         int width = 400;
         int height = 600;
+        /*
         //int leftGap = (int) (width*0.2);
         //int rightGap= (int) (width*0.8);
 
@@ -30,15 +33,35 @@ public class UserInterface {
         addBesitzerBtn.setBounds(20,150,100,40);
         addFahrzeugBtn.setBounds(230,150,100,40);
 
-        System.out.println(besitzerTextField.getX());
-        System.out.println(besitzerTextField.getY());
+        //System.out.println(besitzerTextField.getX());
+        //System.out.println(besitzerTextField.getY());
+        */
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+        l1.addElement("Sebastian");
+        l1.addElement("Mark");
+        l1.addElement("Angela");
+        l1.addElement("Greta");
+        JList<String> besitzerList = new JList<>(l1);
+
+        DefaultListModel<String> l2 = new DefaultListModel<>();
+        l2.addElement("Ferrari");
+        l2.addElement("Mercedes");
+        l2.addElement("Tiger");
+        l2.addElement("Tesla");
+        JList<String> fahrzeugList = new JList<>(l2);
 
         gui.add(besitzerPanel);
         gui.add(fahrzeugPanel);
+
         besitzerPanel.add(besitzerTextField);
         besitzerPanel.add(addBesitzerBtn);
+        besitzerPanel.add(besitzerList);
+        besitzerPanel.add(rmBesitzerBtn);
+
         fahrzeugPanel.add(fahrzeugTextField);
         fahrzeugPanel.add(addFahrzeugBtn);
+        fahrzeugPanel.add(fahrzeugList);
+        fahrzeugPanel.add(rmFahrzeugBtn);
 
         f.add(gui);
         f.setTitle("Fahrzeugverwaltung");
