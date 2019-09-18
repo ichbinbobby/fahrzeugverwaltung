3f3c6de1a81e3f23f8a5fbe0733494f1371b414a
SaikaVA <nthanbinh@gmail.com>
2019-09-18 11:11:48 +0200

    Added JFrame and textFields

--- src/components/gui/UserInterface.java
+++ src/components/gui/UserInterface.java
@@ -5,19 +5,26 @@ import javax.swing.*;
 public class UserInterface {
     public static void main(String[] args) {
         JFrame f = new JFrame();//creating instance of JFrame
+        JPanel gui = new JPanel();
 
-        JTextField besitzer = new JTextField();
-        JTextField fahrzeug = new JTextField();
+        f.add(gui);
 
-        besitzer.setBounds(x: 50, y: 20, width: 100, height: 30);
+        JTextField besitzerTextField = new JTextField();
+        JTextField fahrzeugTextField = new JTextField();
 
-        JButton b = new JButton("click");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        besitzerTextField.setBounds(0,50,100, 40);
+        fahrzeugTextField.setBounds((int) (f.getWidth()*0.8f),50, 100,40);
 
-        f.add(b);//adding button in JFrame
+        gui.add(besitzerTextField);
+        gui.add(fahrzeugTextField);
 
-        f.setSize(400,500);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
+        //JButton b = new JButton("click");//creating instance of JButton
+        //b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+        //f.add(b);//adding button in JFrame
+
+        f.setSize(1280,720);//400 width and 500 height
+        //f.setLayout();//using no layout managers
         f.setVisible(true);//making the frame visible
     }
 }
