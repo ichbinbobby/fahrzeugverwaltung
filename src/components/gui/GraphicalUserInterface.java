ac5cc3f4d540abc48dc80d56e75ec603ff7a32bf
SaikaVA <nthanbinh@gmail.com>
2019-09-21 21:14:45 +0200

    replaced gui class with JavaFX GUI form

---
+++ src/components/gui/GraphicalUserInterface.java
@@ -0,0 +1,17 @@
+package components.gui;
+
+import javax.swing.*;
+
+public class GraphicalUserInterface {
+    private JPanel mainPanel;
+    private JPanel besitzerPanel;
+    private JPanel fahrzeugPanel;
+    private JTextField addBesitzerTextField;
+    private JTextField addFahrzeugTextField;
+    private JButton addBesitzerBtn;
+    private JButton addFahrzeugBtn;
+    private JButton deleteBesitzerBtn;
+    private JButton deleteFahrzeugBtn;
+    private JScrollPane besitzerScrollPane;
+    private JScrollPane FahrzeugScrollPane;
+}
