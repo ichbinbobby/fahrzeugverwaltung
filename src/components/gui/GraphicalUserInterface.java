4762bbb8eb095da6cdf9ed5a00845d17e431de4b
SaikaVA <nthanbinh@gmail.com>
2019-09-21 22:16:11 +0200

    Added main to run the new JavaFX form

--- src/components/gui/GraphicalUserInterface.java
+++ src/components/gui/GraphicalUserInterface.java
@@ -13,5 +13,36 @@ public class GraphicalUserInterface {
     private JButton deleteBesitzerBtn;
     private JButton deleteFahrzeugBtn;
     private JScrollPane besitzerScrollPane;
-    private JScrollPane FahrzeugScrollPane;
+    private JScrollPane fahrzeugScrollPane;
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        GraphicalUserInterface gui = new GraphicalUserInterface();
+
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+        l1.addElement("Sebastian");
+        l1.addElement("Mark");
+        l1.addElement("Angela");
+        l1.addElement("Greta");
+        JList<String> besitzerList = new JList<>(l1);
+        JScrollPane sp1 = new JScrollPane(besitzerList);
+        //gui.besitzerScrollPane.add(sp1);
+
+        /*
+        DefaultListModel<String> l2 = new DefaultListModel<>();
+        l2.addElement("Ferrari");
+        l2.addElement("Mercedes");
+        l2.addElement("Tiger");
+        l2.addElement("Tesla");
+        JList<String> fahrzeugList = new JList<>(l2);
+        //JScrollPane sp2 = new JScrollPane(fahrzeugList);
+        */
+
+        f.setContentPane(gui.mainPanel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setTitle("Fahrzeugverwaltung");
+        f.setSize(400,600);
+        f.setVisible(true);
+    }
 }
