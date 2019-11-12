efdf0164376df7a5bad253a59842bbad2aa7e90e
David Leinweber <davidleinweber@web.de>
2019-11-12 10:27:33 +0100

    Creating value callback for list items.

--- src/components/gui/GraphicalUserInterface.java
+++ src/components/gui/GraphicalUserInterface.java
@@ -1,8 +1,12 @@
 package components.gui;
 
+import models.BesitzerMeta;
+import models.FahrzeugMeta;
+import org.jetbrains.annotations.NotNull;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.lang.reflect.Proxy;
+import java.util.function.Function;
 
 public class GraphicalUserInterface {
     private JPanel mainPanel;
@@ -20,29 +24,32 @@ public class GraphicalUserInterface {
     public GraphicalUserInterface() {
         addBesitzerBtn.addActionListener(e -> System.out.println(addBesitzerTextField.getText()));
         addFahrzeugBtn.addActionListener(e -> System.out.println(addFahrzeugTextField.getText()));
-        deleteBesitzerBtn.addActionListener(e -> System.out.println(((JList<String>)besitzerScrollPane.getViewport().getView()).getSelectedValue()));
-        deleteFahrzeugBtn.addActionListener(e -> System.out.println(((JList<String>)fahrzeugScrollPane.getViewport().getView()).getSelectedValue()));
+        deleteBesitzerBtn.addActionListener(e -> System.out.println(((JList<BesitzerMeta>) besitzerScrollPane.getViewport().getView()).getSelectedValue().getBesitzerId()));
+        deleteFahrzeugBtn.addActionListener(e -> System.out.println(((JList<FahrzeugMeta>) fahrzeugScrollPane.getViewport().getView()).getSelectedValue().getFahrzeugId()));
     }
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
         GraphicalUserInterface gui = new GraphicalUserInterface();
 
-        DefaultListModel<String> l1 = new DefaultListModel<>();
-        l1.addElement("Sebastian");
-        l1.addElement("Mark");
-        l1.addElement("Angela");
-        l1.addElement("Greta");
-        JList<String> besitzerList = new JList<>(l1);
+        DefaultListModel<BesitzerMeta> l1 = new DefaultListModel<>();
+        l1.addElement(new BesitzerMeta(1, "Sebastian"));
+        l1.addElement(new BesitzerMeta(2, "Mark"));
+        l1.addElement(new BesitzerMeta(3, "Angela"));
+        l1.addElement(new BesitzerMeta(4, "Greta"));
+        JList<BesitzerMeta> besitzerList = new JList<>(l1);
+        SetValueCallback(BesitzerMeta.class, besitzerList, BesitzerMeta::getName);
+
         gui.besitzerScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         gui.besitzerScrollPane.setViewportView(besitzerList);
 
-        DefaultListModel<String> l2 = new DefaultListModel<>();
-        l2.addElement("Ferrari");
-        l2.addElement("Mercedes");
-        l2.addElement("Tiger");
-        l2.addElement("Tesla");
-        JList<String> fahrzeugList = new JList<>(l2);
+        DefaultListModel<FahrzeugMeta> l2 = new DefaultListModel<>();
+        l2.addElement(new FahrzeugMeta(1, "Ferrari"));
+        l2.addElement(new FahrzeugMeta(2, "Mercedes"));
+        l2.addElement(new FahrzeugMeta(3, "Tiger"));
+        l2.addElement(new FahrzeugMeta(4, "Tesla"));
+        JList<FahrzeugMeta> fahrzeugList = new JList<>(l2);
+        SetValueCallback(FahrzeugMeta.class, fahrzeugList, FahrzeugMeta::getBezeichnung);
         gui.fahrzeugScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         gui.fahrzeugScrollPane.setViewportView(fahrzeugList);
 
@@ -52,5 +59,21 @@ public class GraphicalUserInterface {
         f.setTitle("Fahrzeugverwaltung");
         f.setSize(400,600);
         f.setVisible(true);
+    }
+
+    private static <T> void SetValueCallback(Class<T> tClass, @NotNull JList<T> besitzerList, Function<T, String> customToString) {
+        ListCellRenderer<? super T> originalCellRenderer = besitzerList.getCellRenderer();
+        besitzerList.setCellRenderer((list, value, index, isSelected, cellHasFocus) -> {
+            Object proxyInstance = Proxy.newProxyInstance(tClass.getClassLoader(), new Class[]{}, (proxy, method, args) -> {
+                if (method.getName().equals("toString")) {
+                    return customToString.apply(value);
+                }
+
+                return method.invoke(value, args);
+            });
+
+            return originalCellRenderer.getListCellRendererComponent(list, (T) proxyInstance, index, isSelected, cellHasFocus);
+
+        });
     }
 }
--- src/models/Fahrzeug.java
+++ src/models/Fahrzeug.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Fahrzeug extends FahrzeugMeta {
+    public Fahrzeug(int fahrzeugId, String bezeichnung) {
+        super(fahrzeugId, bezeichnung);
+    }
+
+    @Override
+    public Fahrzeug setFahrzeugId(int fahrzeugId) {
+        return (Fahrzeug) super.setFahrzeugId(fahrzeugId);
+    }
+
+    @Override
+    public Fahrzeug setBezeichnung(String bezeichnung) {
+        return (Fahrzeug) super.setBezeichnung(bezeichnung);
+    }
+
+
 }
